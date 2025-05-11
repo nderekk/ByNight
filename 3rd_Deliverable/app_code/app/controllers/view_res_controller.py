921bdc15986575ca7a9331ce9e1d47b12afaa0cb
Nikos <nickdereken@gmail.com>
2025-05-11 19:16:28 +0300

    added methods

---
+++ 3rd_Deliverable/app_code/app/controllers/view_res_controller.py
@@ -0,0 +1,14 @@
+from models.reservation import Reservation
+from models.user import User
+from datetime import datetime
+
+class ViewReservationController:
+  def get_upcoming_reservations_for_display(user: User):
+    all_reservations = user.get_reservations()
+    upcoming = [r for r in all_reservations if r.date > datetime.now()]
+    return upcoming
+  
+  def get_past_reservations_for_display(user: User):
+    all_reservations = user.get_reservations()
+    past = [r for r in all_reservations if r.date < datetime.now()]
+    return past
