f16f96ff3879421f1119a0c297c138e6054be8e8
Nikos <nickdereken@gmail.com>
2025-05-11 17:27:24 +0300

    first draft of user

--- 3rd_Deliverable/app_code/app/models/user.py
+++ 3rd_Deliverable/app_code/app/models/user.py
@@ -1,1 +1,29 @@
+from role import Role
+from reservation import Reservation
 
+class User:
+  
+  def __init__(
+      self, 
+      full_name: str,
+      age: int, 
+      role: Role, 
+      phone: int, 
+      email: str, 
+      password: str,
+      reservations: list[Reservation]
+  ):
+    if not isinstance(role, Role):
+      raise ValueError(f"Invalid role: {role}")
+    self.full_name = full_name
+    self.age = age
+    self.role = role
+    self.phone = phone
+    self.email = email
+    self.__password = password
+    self.reservations = reservations
+    
+    
+  def get_reservations():
+    pass
+  
