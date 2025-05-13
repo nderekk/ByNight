020a778ce191d212d69ed46ddb14aa9c0684c3b4
Nikos <nickdereken@gmail.com>
2025-05-13 22:45:37 +0300

    added init

--- 3rd_Deliverable/app_code/main.py
+++ 3rd_Deliverable/app_code/main.py
@@ -1,10 +1,11 @@
 from app.controllers.main_window import MainWindow
-from datetime import datetime
 from PySide6.QtWidgets import QApplication
+from app.data.repositories.init_repos import InitRepos
 import sys
 
 if __name__ == "__main__":
     # upcoming = vr_controller.get_upcoming_reservations_for_display(xx)
+    InitRepos()
      
     app = QApplication(sys.argv)
     window = MainWindow()
