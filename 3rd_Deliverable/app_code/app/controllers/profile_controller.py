1cf160feafb708ad41fe8eb809aaca8bb5a8eecd
elmos <elmosst04@gmail.com>
2025-05-13 22:53:50 +0300

    created profile controller and view profile

---
+++ 3rd_Deliverable/app_code/app/controllers/profile_controller.py
@@ -0,0 +1,24 @@
+from PySide6.QtCore import QObject, Signal
+from app.views.customer_homepage import CustomerHomePage
+from app.controllers.view_res_controller import ViewReservationController
+from app.models.user import User
+from app.models.role import Role
+
+class ProfileController(QObject):
+  # Signals for view updates
+  # view_reservations_pushed = Signal()
+
+  def __init__(self, user: User, show_page: callable):
+    super().__init__()
+    self.user = user
+    self.show_page = show_page
+    self.setup_connections()
+
+  def setup_connections(self):
+    self.view.viewResButton.clicked.connect(self.handle_next_page)
+    
+  def handle_next_page(self):
+    self.view_res_controller = ViewReservationController()
+    self.show_page('customer_view_res_page', self.view_res_controller)
+    
+  
