067f4954310c193ff6036c29c721d9a491933ae1
Nikos <nickdereken@gmail.com>
2025-05-14 01:48:32 +0300

    added contained logic so we dont instantiate the same class twice

--- 3rd_Deliverable/app_code/app/controllers/club_mainpage_controller.py
+++ 3rd_Deliverable/app_code/app/controllers/club_mainpage_controller.py
@@ -1,17 +1,26 @@
 from PySide6.QtCore import QObject
 from app.views.customer_club_mainpage import CustomerClubMainPage
+from app.utils.container import Container
 
 class ClubMainPageController(QObject):
     def __init__(self, show_page: callable):
-
-
         super().__init__()
-        self.view = CustomerClubMainPage()
+        if not Container.is_initialized(CustomerClubMainPage):
+            self.view = CustomerClubMainPage()
+            Container.add_existing_instance(CustomerClubMainPage, self.view)
+        else:
+            self.view = Container.resolve(CustomerClubMainPage)
         self.show_page = show_page
         self.setup_connections()
 
      
     def setup_connections(self):
-            # Connect view signals to controller methods
-        pass
+        # Connect view signals to controller methods
+        self.view.back_btn.clicked.connect(self.handle_back)
+        
+    def handle_back(self):
+        from app.controllers.home_page_controller import HomePageController
+        
+        self.home_page_controller = Container.resolve(HomePageController)
+        self.show_page('customer_home_page', self.home_page_controller)
 
--- 3rd_Deliverable/app_code/app/controllers/home_page_controller.py
+++ 3rd_Deliverable/app_code/app/controllers/home_page_controller.py
@@ -4,6 +4,8 @@ from app.controllers.view_res_controller import ViewReservationsController
 from app.controllers.club_mainpage_controller import ClubMainPageController
 from app.models.user import User
 from app.models.role import Role
+from app.utils.container import Container
+from app.data.repositories.club_repository import ClubRepository
 
 class HomePageController(QObject):
   # Signals for view updates
@@ -12,23 +14,32 @@ class HomePageController(QObject):
   def __init__(self, user: User, show_page: callable):
     super().__init__()
     self.user = user
+    self.clubs = Container.resolve(ClubRepository).get_all()
     self.show_page = show_page
     if self.user.role == Role.CUSTOMER:
-      self.view = CustomerHomePage()
+      self.view = CustomerHomePage(self.clubs)
     else:
       print("KANE TO GUI TBOY")
     self.setup_connections()
 
   def setup_connections(self):
-    self.view.viewResButton.clicked.connect(self.handle_next_page)
+    self.view.viewResButton.clicked.connect(self.hand_view_res)
     self.view.more_button.clicked.connect(self.handle_club_mainpage)
     
-  def handle_next_page(self):
-    self.view_res_controller = ViewReservationsController()
+  def hand_view_res(self):
+    if not Container.is_initialized(ClubMainPageController):
+      self.view_res_controller = ViewReservationsController()
+      Container.add_existing_instance(ViewReservationsController, self.view_res_controller)
+    else:
+      self.view_res_controller = Container.resolve(ViewReservationsController)
     self.show_page('customer_view_res_page', self.view_res_controller)
     
   def handle_club_mainpage(self):
-    self.club_mainpage_controller= ClubMainPageController(self.show_page)
+    if not Container.is_initialized(ClubMainPageController):
+      self.club_mainpage_controller = ClubMainPageController(self.show_page)
+      Container.add_existing_instance(ClubMainPageController, self.club_mainpage_controller)
+    else:
+      self.club_mainpage_controller = Container.resolve(ClubMainPageController)
     self.show_page('customer_club_main_page', self.club_mainpage_controller)
     
   
