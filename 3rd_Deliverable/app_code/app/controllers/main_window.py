8b6ad6a221db8dd860363e3a939118972a989e37
Nikos <nickdereken@gmail.com>
2025-05-13 18:04:28 +0300

    refactored to non specific show_page method

--- 3rd_Deliverable/app_code/app/controllers/main_window.py
+++ 3rd_Deliverable/app_code/app/controllers/main_window.py
@@ -3,7 +3,8 @@ from app.controllers.login_controller import LoginController
 from app.controllers.home_page_controller import HomePageController
 from app.services.auth_service import AuthService
 from app.data.repositories.user_repository import UserRepository
-from PySide6.QtWidgets import QStackedWidget, QMainWindow, QWidget
+from PySide6.QtWidgets import QStackedWidget, QMainWindow
+from PySide6.QtCore import QObject, Signal
 
 class MainWindow(QMainWindow):
   def __init__(self):
@@ -21,35 +22,41 @@ class MainWindow(QMainWindow):
     # set up controllers
     self.user_repo = UserRepository()
     self.auth_service = AuthService(self.user_repo)
-    self.login_controller = LoginController(self.auth_service, self.show_customer_home_page)
+    self.login_controller = LoginController(self.auth_service, self.show_page)
     
-    self.home_page_controller = HomePageController(self.show_customer_view_reservations)
+    
     
     self.view_res_controller = ViewReservationController()
     
     
      
     # Start with login page
-    self.show_login()
+    self.show_page('login_page', self.login_controller)
 
-  def show_login(self):
-    if 'login_page' not in self.pages:
-      self.login_page = self.login_controller.view
-      self.stack.addWidget(self.login_page)
-      self.pages['login_page'] = self.login_page
-    self.stack.setCurrentWidget(self.login_page)
+  def show_page(self, page_name: str, page_controller: QObject):
+    if page_name not in self.pages:
+      self.pages[page_name] = page_controller.view
+      self.stack.addWidget(self.pages[page_name])
+    self.stack.setCurrentWidget(self.pages[page_name])
+
+  # def show_login(self):
+  #   if 'login_page' not in self.pages:
+  #     self.login_page = self.login_controller.view
+  #     self.stack.addWidget(self.login_page)
+  #     self.pages['login_page'] = self.login_page
+  #   self.stack.setCurrentWidget(self.login_page)
     
-  def show_customer_home_page(self):
-    if 'customer_home_page' not in self.pages:
-      self.customer_home_page = self.home_page_controller.view
-      self.stack.addWidget(self.customer_home_page)
-      self.pages['customer_home_page'] = self.customer_home_page
-    self.stack.setCurrentWidget(self.customer_home_page)
+  # def show_customer_home_page(self):
+  #   if 'customer_home_page' not in self.pages:
+  #     self.customer_home_page = self.home_page_controller.view
+  #     self.stack.addWidget(self.customer_home_page)
+  #     self.pages['customer_home_page'] = self.customer_home_page
+  #   self.stack.setCurrentWidget(self.customer_home_page)
 
-  def show_customer_view_reservations(self):
-    if 'customer_view_res_page' not in self.pages:
-      self.customer_view_res_page = self.view_res_controller.view
-      self.stack.addWidget(self.customer_view_res_page)
-      self.pages['customer_view_res_page'] = self.customer_view_res_page
-    self.stack.setCurrentWidget(self.customer_view_res_page)
+  # def show_customer_view_reservations(self):
+  #   if 'customer_view_res_page' not in self.pages:
+  #     self.customer_view_res_page = self.view_res_controller.view
+  #     self.stack.addWidget(self.customer_view_res_page)
+  #     self.pages['customer_view_res_page'] = self.customer_view_res_page
+  #   self.stack.setCurrentWidget(self.customer_view_res_page)
   
