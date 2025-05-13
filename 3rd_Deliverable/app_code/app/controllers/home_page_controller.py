72ef313e4c5e71f2562f0a885d365240ff3d2007
Nikos <nickdereken@gmail.com>
2025-05-13 22:46:59 +0300

    go from home to view res

--- 3rd_Deliverable/app_code/app/controllers/home_page_controller.py
+++ 3rd_Deliverable/app_code/app/controllers/home_page_controller.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import QObject, Signal
 from app.views.customer_homepage import CustomerHomePage
-from app.controllers.view_res_controller import ViewReservationController
+from app.controllers.view_res_controller import ViewReservationsController
 from app.models.user import User
 from app.models.role import Role
 
@@ -22,7 +22,7 @@ class HomePageController(QObject):
     self.view.viewResButton.clicked.connect(self.handle_next_page)
     
   def handle_next_page(self):
-    self.view_res_controller = ViewReservationController()
+    self.view_res_controller = ViewReservationsController()
     self.show_page('customer_view_res_page', self.view_res_controller)
     
   
