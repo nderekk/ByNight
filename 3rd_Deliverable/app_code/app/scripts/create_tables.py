85b0ec09631ea9d91b775017c46f277b25d44981
nderekk <nickdereken@gmail.com>
2025-05-15 23:43:00 +0300

    mikroallages slet

--- 3rd_Deliverable/app_code/app/scripts/create_tables.py
+++ 3rd_Deliverable/app_code/app/scripts/create_tables.py
@@ -1,6 +1,6 @@
 from app.data.database import Base, engine
-from models.user import User
-from models.reservation import Reservation
-from models.club import Club
+import app.models
+
+# PYTHONPATH=. python app/scripts/create_tables.py
 
 Base.metadata.create_all(bind=engine)
