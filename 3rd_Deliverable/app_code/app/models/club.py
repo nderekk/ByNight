dfd168c9f02d6ec6543bf7a845e63cd35ee3a8c2
Nikos <nickdereken@gmail.com>
2025-05-16 19:53:06 +0300

    added method to get filter options

--- 3rd_Deliverable/app_code/app/models/club.py
+++ 3rd_Deliverable/app_code/app/models/club.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import relationship
 from app.data.database import Base
 from app.utils.container import Container
 from app.services.db_session import DatabaseSession
-import app.models
+from sqlalchemy import distinct
 
 class Club(Base):
   __tablename__ = "clubs"
 
   id = Column(Integer, primary_key=True)
   name = Column(String, unique=True, nullable=False)
+  address = Column(String, nullable=False)
   location = Column(String, nullable=False)
   manager = Column(String, nullable=False)
 
@@ -25,3 +26,14 @@ class Club(Base):
   def get_clubs_all(cls):
     session = Container.resolve(DatabaseSession)
     return session.query(Club).all()
+  
+  @classmethod
+  def get_club_filters(cls):
+    from app.models import Event
+    
+    session = Container.resolve(DatabaseSession)
+    return {
+      "location": [loc for (loc,) in session.query(distinct(Club.location)).order_by(Club.location).all()],
+      "music": [music for (music,) in session.query(distinct(Event.music)).order_by(Event.music).all()],
+      "event": [title for (title,) in session.query(distinct(Event.title)).order_by(Event.title).all()],
+    }
