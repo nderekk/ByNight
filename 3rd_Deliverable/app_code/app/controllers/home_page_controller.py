7dd6323f5e3506a13827d65ee681cee0b13405c6
Nikos <nickdereken@gmail.com>
2025-05-17 03:17:34 +0300

    moved import down

--- 3rd_Deliverable/app_code/app/controllers/home_page_controller.py
+++ 3rd_Deliverable/app_code/app/controllers/home_page_controller.py
@@ -1,6 +1,5 @@
 from PySide6.QtCore import QObject, Signal
 from app.views import CustomerHomePage
-from app.controllers import ViewReservationsController
 from app.controllers import ClubMainPageController
 from app.models import User, Role, Club
 from app.utils.container import Container
@@ -27,6 +26,8 @@ class HomePageController(QObject):
     self.view.searchLineEdit.textChanged.connect(self.apply_filters)
     
   def hand_view_res(self):
+    from app.controllers import ViewReservationsController
+    
     if not Container.is_initialized(ViewReservationsController):
       self.view_res_controller = ViewReservationsController(self.show_page)
       Container.add_existing_instance(ViewReservationsController, self.view_res_controller)
