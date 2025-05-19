7db94a638eaeca0ad087a5a2739698ce89da160b
Nikos <nickdereken@gmail.com>
2025-05-20 02:51:51 +0300

    me hardcoded times so far that allaksei auto

---
+++ 3rd_Deliverable/app_code/app/controllers/staff_homepage_controller.py
@@ -0,0 +1,30 @@
+from PySide6.QtCore import QObject, Signal
+from app.models import Club
+from app.views import StaffHomePage, MessagePopup
+
+class StaffHomePageController(QObject):
+  def __init__(self, show_page: callable):
+    super().__init__()
+    self.show_page = show_page
+    reservations_data = [
+    {"id": 1, "full_name": "Alice Johnson"},
+    {"id": 102, "full_name": "Bob Smith"},
+    {"id": 103, "full_name": "Carla Gomez"},
+    ]
+    self.view = StaffHomePage(reservations_data)
+    self.setup_connections()
+
+  def setup_connections(self):
+    self.view.camera_clicked.connect(self.handle_qrcode)
+    
+  def handle_qrcode(self):
+    from app.utils import decode_qr
+    detected_id = decode_qr()
+    if detected_id in self.view.cards.keys() and detected_id != -1:
+      card = self.view.cards.get(detected_id)
+      if card:
+        card.checkbox.setChecked(True)
+        popup = MessagePopup(success=True, message=f"Reservation {detected_id}\nChecked In")
+        popup.exec()
+        # TODO notification code
+        
