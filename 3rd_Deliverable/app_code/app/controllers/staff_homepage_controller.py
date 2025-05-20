3008a443776df39ad55431e5654195d295a407d9
Nikos <nickdereken@gmail.com>
2025-05-20 03:47:59 +0300

    evala code gia fetch club's reservations, theoritika sosto alla exo thema me ta models inheritance

--- 3rd_Deliverable/app_code/app/controllers/staff_homepage_controller.py
+++ 3rd_Deliverable/app_code/app/controllers/staff_homepage_controller.py
@@ -1,16 +1,27 @@
 from PySide6.QtCore import QObject, Signal
-from app.models import Club
+from app.models import Staff
 from app.views import StaffHomePage, MessagePopup
+from app.utils import Container
 
 class StaffHomePageController(QObject):
   def __init__(self, show_page: callable):
     super().__init__()
     self.show_page = show_page
+    self.user = Container.resolve(Staff)
+    reservations = self.user.get_upcoming_club_reservations()
+    print(reservations)
     reservations_data = [
-    {"id": 1, "full_name": "Alice Johnson"},
-    {"id": 102, "full_name": "Bob Smith"},
-    {"id": 103, "full_name": "Carla Gomez"},
-    ]
+      {
+        "id": res.id,
+        "full_name": self.user.full_name
+      } 
+      for res in reservations
+    ] 
+    # reservations_data = [
+    # {"id": 1, "full_name": "Alice Johnson"},
+    # {"id": 102, "full_name": "Bob Smith"},
+    # {"id": 103, "full_name": "Carla Gomez"},
+    # ]
     self.view = StaffHomePage(reservations_data)
     self.setup_connections()
 
